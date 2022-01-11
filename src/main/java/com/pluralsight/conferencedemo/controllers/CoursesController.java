ee3a8f794cc8cf5b38cc28ef506928f1538915fe
Sidney Jones <sjones.presence@gmail.com>
2022-01-11 13:14:30 -0500

    added CORS policy to authors. added create, delete, and put methods to course controller.

--- src/main/java/com/pluralsight/conferencedemo/controllers/CoursesController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/CoursesController.java
@@ -4,6 +4,7 @@ import com.pluralsight.conferencedemo.models.Author;
 import com.pluralsight.conferencedemo.models.Course;
 import com.pluralsight.conferencedemo.repositories.AuthorRepository;
 import com.pluralsight.conferencedemo.repositories.CourseRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,4 +23,21 @@ public class CoursesController {
     @GetMapping
     @RequestMapping("{id}")
     public Course get(@PathVariable Long id) { return courseRepository.getById(id); }
+
+    @PostMapping
+    public Course create(@RequestBody final Course course) {
+        return courseRepository.saveAndFlush(course);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        courseRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Course update(@PathVariable Long id, @RequestBody Course course) {
+        Course existingCourse = courseRepository.getById(id);
+        BeanUtils.copyProperties(course, existingCourse);
+        return courseRepository.saveAndFlush(existingCourse);
+    }
 }
