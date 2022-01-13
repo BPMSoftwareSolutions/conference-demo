162b5132b4eed1decee8de4364f5b123d2212126
BPMSoftwareSolutions <sjones@bpmsoftwaresolutions.com>
2022-01-13 07:22:29 -0500

    Revert "Merging changes."
    This reverts commit adf4675da3a811dbe3293d16749c1a287bb1e7ed.

--- src/main/java/com/pluralsight/conferencedemo/controllers/AuthorsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/AuthorsController.java
@@ -4,12 +4,14 @@ import com.pluralsight.conferencedemo.models.Author;
 import com.pluralsight.conferencedemo.models.Course;
 import com.pluralsight.conferencedemo.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin
 @RestController
 @RequestMapping("api/v1/authors")
 public class AuthorsController {
